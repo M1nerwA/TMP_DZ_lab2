510f285d87512a690e23868272bcfc88d4b231ef
M1nerwA <kyrmaevivan06gmail.com>
2025-04-18 00:23:17 +0300

    new style Mozilla Part3 punkt 6

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 #include <iostream>//Подключаем библиотеки (Попытка №2), будем считать английские комиты за исходную версию)
+=======
+#include <iostream>//Подключаем библиотеки (Попытка №2), будем считать английские комиты за исходную версию)
+>>>>>>> ac7f52d (new style Mozilla)
 #include <string>
 
-int main(){
-    std::string name = "";//Username
-    std::cin >> name;//Insert username
-    std::cout << "Hello world from " << name << std::end;//print
+int
+main()
+{
+  std::string name = "";                                // Username
+  std::cin >> name;                                     // Insert username
+  std::cout << "Hello world from " << name << std::end; // print
 }
