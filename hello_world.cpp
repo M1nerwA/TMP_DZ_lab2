25986863507623e022fbfa72c74241b2302d68e4
M1nerwA <kyrmaevivan06@gmail.com>
2025-04-18 00:54:15 +0300

    Update Part3 punkt4 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
-#include <iostream>//Подключаем библиотеки (Попытка №2), будем считать английские комиты за исходную версию)
+#include <iostream>//Подключаем библиотеки (Попытка №3), будем считать английские комиты за исходную версию)
 #include <string>
 
 int main(){
-    std::string name = "";//Username
+    std::string name = "";//Имя пользователя (Попытка 3 пункт 4)
     std::cin >> name;//Insert username
     std::cout << "Hello world from " << name << std::end;//print
 }
