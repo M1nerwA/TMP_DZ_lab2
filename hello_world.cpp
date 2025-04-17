c8d5b7bbbfa0b5332d3be34cf81c0a4158e70bca
M1nerwA <kyrmaevivan06@gmail.com>
2025-04-17 22:37:12 +0300

    new style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
-#include <iostream>//Подключение библиотек
+#include <iostream> //Подключение библиотек
 #include <string>
 
-int main(){
-    std::string name = "";//переменная имя поьзователя
-    std::cin >> name;//Ввод имени пользователя
-    std::cout << "Hello world from " << name << std::end;//Вывод
+int
+main()
+{
+  std::string name = ""; // переменная имя поьзователя
+  std::cin >> name;      // Ввод имени пользователя
+  std::cout << "Hello world from " << name << std::end; // Вывод
 }
