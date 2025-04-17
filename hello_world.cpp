4f5d15caf2e946c9ebf8efb88ea016dfe9aeddc5
M1nerwA <kyrmaevivan06@gmail.com>
2025-04-17 23:48:02 +0300

    Попытка

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
-#include <iostream> //Подключение библиотек
+#include <iostream>//include libraries
 #include <string>
 
-int main()
-{
-  std::string name = ""; // переменная имя поьзователя
-  std::cin >> name;      // Ввод имени пользователя
-  std::cout << "Hello world from " << name << std::end; // Вывод
+int main(){
+    std::string name = "";//Username
+    std::cin >> name;//Insert username
+    std::cout << "Hello world from " << name << std::end;//print
+>>>>>>> cdd7c4e7cc6813c21234b025d460e9b41db6bd96
 }
